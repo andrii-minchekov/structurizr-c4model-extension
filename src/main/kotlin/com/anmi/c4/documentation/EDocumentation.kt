6b4c8ffb663dcdced10922642f2b7a07a8d3767d
Andrii Minchekov <andrii.minchekov@gmail.com>
2023-10-12 19:41:41 +0300

    Resolve images from each System folder

--- src/main/kotlin/com/anmi/c4/documentation/EDocumentation.kt
+++ src/main/kotlin/com/anmi/c4/documentation/EDocumentation.kt
@@ -8,28 +8,30 @@ import java.io.File
 import java.net.URL
 
 
+private const val DOC_BASE_DIR = "documentation"
+
 class EDocumentation(workspace: Workspace) {
 
     private val template = AutomaticDocumentationTemplate(workspace)
 
     init {
-        addImages()
         workspace.model.softwareSystems.forEach { ss ->
             workspace.views.containerViews.firstOrNull() { view -> view.softwareSystem.name == ss.name }?.let {
+                addImages("$DOC_BASE_DIR/${it.key}")
                 doc(ss, it)
             }
         }
     }
 
     private fun doc(system: SoftwareSystem, containerView: ContainerView) {
-        val docRootURL: URL? = ClassLoader.getSystemResource("documentation/${containerView.key}")
+        val docRootURL: URL? = ClassLoader.getSystemResource("$DOC_BASE_DIR/${containerView.key}")
         if (docRootURL != null && docRootURL.protocol == "file") {
             template.addSections(system, File(docRootURL.toURI()))
         }
     }
 
-    private fun addImages() {
-        val imageUrl = ClassLoader.getSystemResource("documentation/images")
+    private fun addImages(imagePathOfSystem: String) {
+        val imageUrl = ClassLoader.getSystemResource(imagePathOfSystem)
         if (imageUrl != null && imageUrl.protocol == "file") {
             template.addImages(File(imageUrl.toURI()))
         }
